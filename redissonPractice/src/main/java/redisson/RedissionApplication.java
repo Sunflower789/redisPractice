40b03d3a42fec2219db3a5515beb05e2df69f784
Sunflower789 <sunflower_hx_9@163.com>
2024-02-27 16:16:00 +0800

    从官方git拉取代码进行初始化测试

---
+++ redissonPractice/src/main/java/redisson/RedissionApplication.java
@@ -0,0 +1,14 @@
+package redisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class RedissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedissionApplication.class, args);
+	}
+
+}
